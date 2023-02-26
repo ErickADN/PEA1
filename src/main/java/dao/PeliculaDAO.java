3ed3084b2167a79f9c013bec2e74b71c9c4a9a60
Jessica <Jessica@DESKTOP-GVB16UV>
2023-02-26 00:21:22 -0500

    Realizando peliculadao junto a sus metodos

---
+++ src/main/java/dao/PeliculaDAO.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Pelicula;
+
+public class PeliculaDAO {
+	db.dB db= new db.dB("mysql8003.site4now.net","a95010_cinesta","cinestar1234","db_a95010_cinesta");
+	/*public Object getPeliculas(int id) {
+		
+		db.Sentencia(String.format("call sp_getPeliculas(%s)",id));
+		return db.getRegistros();
+	}*/
+	/*La siguiente forma es para Lista*/
+	public Object getPeliculas(int id,boolean bLista) {
+		
+		db.Sentencia(String.format("call sp_getPeliculas(%s)",id));
+		String[][] peliculas = db.getRegistros();
+		
+		if(peliculas==null) return null;
+		if(!bLista) return peliculas;
+		
+		List<Pelicula> lstPeliculas = new ArrayList<>();
+		for(String[] pelicula : peliculas)
+			lstPeliculas.add(new Pelicula(pelicula));
+		return lstPeliculas;
+	}
+	
+	public Object getPelicula(String id, boolean bEntidad) {
+		db.Sentencia(String.format("call sp_getPelicula(%s)",id));
+		String[] aRegistro = db.getRegistro();
+		
+		if(aRegistro==null) return null;
+		db.Sentencia(String.format("select getGenerosDetalle('%s')",aRegistro[4]));
+		aRegistro[4]=db.getCampo();
+		if(!bEntidad) return aRegistro;
+		
+		
+		return new Pelicula (aRegistro);
+	}
+
+}
